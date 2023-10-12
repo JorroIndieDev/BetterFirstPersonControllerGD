1ea064c2c08f6e9fe48bab836779d68c2fee4139
Jorro <goliveirajr02@gmail.com>
2023-10-12 22:33:23 +0100

    small cleanup
    Cleaned up unkown numbers and removed unused vars

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@
 --------------------------------------------------------------------------------
 	9/28/2024 - Jorro total linhas: 231
 	
-	vars used in project on @export Gravity = 17 Jump_Velocity = 8
+	vars used in project on @export Gravity = 17 Jump_Force = 8
 	
 To do:
 	values multiplied by delta need to be changed for @export vars
@@ -22,12 +22,15 @@ To do:
 	-wall jump
 	-ramp movement
 	for now this is all
+Currently doing:
+	verefying the code and cleaning up any "floating" variables and numbers cleaning and making sure
+	the code is readable, leave feedback so i can keep improving this controller
 '''
 
 
 extends CharacterBody3D
 
-# HUD All @onready vars under #HUD are not needed for any movement of the character These vars only influence the GUI for debugging
+# HUD 
 @onready var debug_screen = $Head/Camera3D/debug_screen
 @onready var debug_speed = $Head/Camera3D/debug_screen/Speed
 @onready var debug_velocity = $Head/Camera3D/debug_screen/Velocity
@@ -43,13 +46,16 @@ extends CharacterBody3D
 @onready var speed_setting = $Head/Camera3D/Settings/Speed/LineEdit3
 @onready var sprint_setting = $Head/Camera3D/Settings/SprintSPeed/LineEdit4
 @onready var flex3_setting = $Head/Camera3D/Settings/Flex3
+# All @onready vars under #HUD are not needed for any movement of the character These vars only influence the GUI for debugging
 
 var _basis = Basis()
 
 # Movement Vars
 var current_speed = 0 
-var SPRINTING_SPEED = 9.0
-var WALKING_SPEED = 6.0
+## max sprinting speed
+@export var SPRINTING_SPEED = 9.0
+## max walking speed
+@export var WALKING_SPEED = 6.0
 
 @export var gravity :float # 16
 @onready var ground_check = $GroundCheck
@@ -57,8 +63,8 @@ var fall_contact = false
 
 # Jump Vars
 @onready var can_jump = true
-@export var jump_delay :float # 0.25
-@export var jump_velocity :float # 6
+## Jumping heigh, default as 6 (float)
+@export var jump_force :float # 6
 @onready var coyote_jump :bool = true
 
 # Head bobing
@@ -73,24 +79,28 @@ var velocity_clamped
 var target_fov
 
 # Camera vars
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
+## Takes a Node or Node2D as parent of a Camera3D Node
+@export var head: Node
+## Takes a Camera3D node
+@export  var camera: Node
 @onready var original_camera_pos :Vector3 = camera.transform.origin
 var camera_rotation = Vector3.ZERO
-var mouse_sense = 0.06
-var look_rot = Vector3.ZERO
+## changing this value is not advised, if needed change very small quanteties (0.01)
+@export var mouse_sense = 0.06
+var look_rotation = Vector3.ZERO
 var move_dir = Vector3.ZERO
 var fov_change_enable = true
 var tilt_axis = Vector3()
 var tilt_angle = deg_to_rad(45)
 var tilt_amount = deg_to_rad(15.0) # Adjust this value to control the tilt amount
+var max_look_amount = 80
 
 
 func _ready():
 	current_speed = WALKING_SPEED
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	gravity_setting.placeholder_text = str(gravity)
-	jump_setting.placeholder_text = str(jump_velocity)
+	jump_setting.placeholder_text = str(jump_force)
 	speed_setting.placeholder_text = str(WALKING_SPEED)
 	sprint_setting.placeholder_text = str(SPRINTING_SPEED)
 
@@ -104,9 +114,9 @@ func _ready():
 func _input(event):
 
 	if event is InputEventMouseMotion and  Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		look_rot.y -= (event.relative.x * mouse_sense)
-		look_rot.x -= (event.relative.y * mouse_sense)
-		look_rot.x = clamp(look_rot.x, -80, 80)
+		look_rotation.y -= (event.relative.x * mouse_sense)
+		look_rotation.x -= (event.relative.y * mouse_sense)
+		look_rotation.x = clamp(look_rotation.x, -max_look_amount, max_look_amount)
 
 
 	if Input.is_action_just_pressed("ESC"):
@@ -119,13 +129,13 @@ func _physics_process(delta):
 
 
 	# Head Rotation
-	head.rotation_degrees.x = look_rot.x
-	rotation_degrees.y = look_rot.y
+	head.rotation_degrees.x = look_rotation.x
+	rotation_degrees.y = look_rotation.y
 
 
 	# Movement CHANGE
 	var direction = Vector3(Input.get_axis("Left", "Right"), 0, Input.get_axis("Forward", "Backwards")).normalized().rotated(Vector3.UP, rotation.y)
-	debug_flex2.text = str(direction)
+	debug_flex2.text = str(direction) #not needed
 
 	# Sprint
 	if Input.is_action_pressed("Sprint"):
@@ -145,7 +155,7 @@ func _physics_process(delta):
 	# Jump
 	if is_on_floor() and is_jumping:
 		velocity.y = 0
-		velocity.y = jump_velocity
+		velocity.y = jump_force
 		can_jump = false
 		reset_jump()
 	
@@ -153,7 +163,7 @@ func _physics_process(delta):
 	# Grounded #HASDEBUG
 	if is_on_floor():
 		coyote_jump = true
-		debug_flex1.text = "null"
+		debug_flex1.text = "null" #not needed
 		if direction:
 			velocity.x = direction.x * current_speed
 			velocity.z = direction.z * current_speed
@@ -187,11 +197,11 @@ func _physics_process(delta):
 	tilt_axis = Input.get_action_strength("Left") - Input.get_action_strength("Right")
 	camera.rotation_degrees.z = clamp(camera.rotation_degrees.z + tilt_axis * tilt_amount, -tilt_angle, tilt_angle)
 	camera_rotation = camera.rotation_degrees.z
-	debug_flex3.text = str(camera.rotation_degrees.z) + " tilt"
+	debug_flex3.text = str(camera.rotation_degrees.z) + " tilt" #not needed
 
-	# Reset camera rotation if no movement keys are pressed
+	# Reset camera rotation if no movement keys are pressed #HASDEBUG
 	if tilt_axis == 0:
-		debug_flex3.text = str(camera.rotation_degrees.z) + " stopped moving"
+		debug_flex3.text = str(camera.rotation_degrees.z) + " stopped moving" #not needed
 		camera.rotation_degrees.z = lerp(camera_rotation,0.0,delta * 15)  # Reset rotation to zero
 
 
@@ -199,7 +209,7 @@ func _physics_process(delta):
 	debug_speed.text = "speed: " + str(current_speed)
 	debug_velocity.text = "velocity: " + str(velocity)
 	debug_FOV.text = "FOV: " + str(camera.fov)
-
+	#not needed
 
 	move_and_slide()
 
@@ -217,16 +227,16 @@ func reset_jump():
 
 func coyoteTime(): #HASDEBUG
 	await get_tree().create_timer(.1).timeout
-	debug_flex1.text = "COYOTE"
+	debug_flex1.text = "COYOTE" #not needed
 	coyote_jump = false
 
-
+# Settings Menu signals
 func ChangeGravity(value):
 	gravity = value
 
 
 func ChangeJump(value):
-	jump_velocity = value
+	jump_force = value
 
 
 func ChangeSpeed(value):
